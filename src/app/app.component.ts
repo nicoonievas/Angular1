9d853166d10b72d55401fd5089ecf66d9feb7af5
nicoonievas <nicolasnievas1@gmail.com>
2024-05-23 21:07:37 -0300

    correcciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Component2Component } from './pages/home/component2/component2.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Component2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
